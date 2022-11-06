fa745c1dc66463ee553bc8c3ce53c932ae3d7722
Danilo Koerber (FG-814) <danilo.koerber@bmwgroup.com>
2022-11-06 20:28:01 +0100

    Removed tests

--- src/test/java/io/easyware/GreetingResourceIT.java
+++
@@ -1,9 +0,0 @@
-package io.easyware;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-public class GreetingResourceIT extends GreetingResourceTest {
-
-    // Execute the same tests but in native mode.
-}
